1845e2a72fa05789d3766f06d067c4730d840f78
Urinbasarov Dias <jentle.men29@gmail.com>
2018-11-12 19:26:39 +0600

    do

---
+++ ConectedLevelHomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ConectedLevelHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection())
+            {
+                string connectionString = ConfigurationManager
+                    .ConnectionStrings["GroupConnString"].ConnectionString;
+
+                sqlConnection.ConnectionString = connectionString;
+
+                SqlCommand createDataBaseCommand = new SqlCommand();
+                createDataBaseCommand.Connection = sqlConnection;
+                createDataBaseCommand.CommandText = ("CREATE DATABASE UNIVER");
+                createDataBaseCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
